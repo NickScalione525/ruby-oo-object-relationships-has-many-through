215a0c73d5762e27d41851132dfb19ecb5ea5592
nick <nicholas_scalione@yahoo.com>
2021-01-26 23:36:18 -0500

    commit tree d8b80792ea86fb632050f3f068350df91fe4020e

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,27 @@
 class Waiter
+attr_accessor :name, :experience
+
+@@all =[]
+
+def initialize(name, experience)
+    @name = name
+    @experience = experience
+    @@all << self
+end
+
+def new_meal(customer, tip, total)
+        Meal.new(self, customer, total, tip)
+end
+def meals
+    Meal.all.select do |meal|
+        meal.waiter == self
+    end
+end
+def self.all
+    @@all
+end
+def best_tipper
+    self.meals.max_by {|meal|meal.tip}.customer
+end
 
 end
